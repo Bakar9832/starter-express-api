bf7dc5130fbf8b6a0c9585744a684dd5687ae5c5
Abubakar98 <bakar9832@gmail.com>
2023-09-01 01:54:36 +0500

    new commit

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+const express = require('express');
+const cors = require('cors');
+const { default: axios } = require('axios');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+    try {
+        const response = axios.put('https://api.chatengine.io/users/', { username: username, secret: username, first_name: username },
+            { headers: { 'private-key': "afdf7bae-f7d6-4732-8d2b-e1a6d27ecbec" } });
+        return res.status((await response).status).json((await response).data);
+    } catch (error) {
+        return res.status(error.response.status).json(error.response.data)
+    }
 })
-app.listen(process.env.PORT || 3000)
+
+const PORT = 3001 || process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log("server is listening on port: ", PORT);
+})
